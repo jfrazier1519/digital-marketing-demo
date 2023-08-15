94e4890eaf054d35f92d8905e072f4fc1b55b989
Joshua-Frazier <joshua.frazier1519@gmail.com>
2023-08-14 20:28:56 -0600

    fix(products): resolved changes for code review

---
+++ reefer_review_mobile/lib/presentation/post/event_post_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:expandable_text/expandable_text.dart';
+import '../../presentation/shared/rounded_container.dart';
+import '../../data/post/event_post.dart';
+
+class EventPostWidget extends StatelessWidget {
+  final EventPost eventPost;
+
+  EventPostWidget({required this.eventPost});
+
+  @override
+  Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
+    return RoundedContainer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(eventPost.author.profileImageUrl),
+                  radius: 25,
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        eventPost.author.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      ExpandableText(
+                        eventPost.content,
+                        expandText: 'See More',
+                        collapseText: 'See Less',
+                        maxLines: 2,
+                        linkColor: colorScheme.primary,
+                      ),
+                      const SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(eventPost.eventUrl);
+                        },
+                        child: Text(
+                          'Go To Event',
+                          style: TextStyle(
+                            color: colorScheme.primary,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          eventPost.image.isNotEmpty
+              ? SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: Image.asset(
+                    eventPost.image,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
---
+++ reefer_review_mobile/lib/presentation/post/post_to_widget_converter.dart
@@ -0,0 +1,33 @@
+// post_to_widget_converter.dart
+
+import 'package:flutter/material.dart';
+import 'package:reefer_review_mobile/presentation/post/product_post_widget.dart';
+import 'package:reefer_review_mobile/presentation/post/review_post_widget.dart';
+import '../../data/post/event_post.dart';
+import '../../data/post/general_post.dart';
+import '../../data/post/loyalty_post.dart';
+import '../../data/post/post.dart';
+import '../../data/post/product_post.dart';
+import '../../data/post/review_post.dart';
+import './event_post_widget.dart';
+import 'general_post_widget.dart';
+import 'loyalty_post_widget.dart';
+
+class PostToWidgetConverter {
+  static Widget convert(Post post) {
+    switch (post.runtimeType) {
+      case GeneralPost:
+        return GeneralPostWidget(generalPost: post as GeneralPost);
+      case EventPost:
+        return EventPostWidget(eventPost: post as EventPost);
+      case LoyaltyPost:
+        return LoyaltyPostWidget(loyaltyPost: post as LoyaltyPost);
+      case ProductPost:
+        return ProductPostWidget(productPost: post as ProductPost);
+      case ReviewPost:
+        return ReviewPostWidget(reviewPost: post as ReviewPost);
+      default:
+        return const Text('Unknown post type');
+    }
+  }
+}
