ac83c045b2915dcbf2430df2c0697247b83324a5
Joshua-Frazier <joshua.frazier1519@gmail.com>
2023-08-16 21:06:37 -0600

    feat(productDetails): added nav bar

--- reefer_review_mobile/lib/presentation/products_screen/products_details_screen/products_details_screen.dart
+++ reefer_review_mobile/lib/presentation/products_screen/products_details_screen/products_details_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reefer_review_mobile/repositories/review_repository/fake_review_repository_impl.dart';
-import 'package:reefer_review_mobile/repositories/review_repository/review_repository.dart';
 
 import '../../../bloc/review_bloc/review_bloc.dart';
 import '../../../data/models/product/product.dart';
 import '../../review/product_review_widget.dart';
+import '../../shared/bottom_nav_bar.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
   final Product product;
 
   const ProductDetailsScreen({required this.product});
+
+  @override
+  _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  int _currentIndex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -18,156 +25,145 @@ class ProductDetailsScreen extends StatelessWidget {
 
     return BlocProvider(
       create: (context) => ReviewBloc(FakeReviewRepository())
-        ..add(FetchReviewsForProduct(product.productId)),
+        ..add(FetchReviewsForProduct(widget.product.productId)),
       child: Scaffold(
-          appBar: AppBar(),
-          body: ListView(children: [
-            // Product Image
-            if (product.image.isNotEmpty)
-              Container(
-                height: 150.0, // Set the desired height here
-                child: Image.asset(product.image, fit: BoxFit.cover),
-              ),
-
-            // The rest of the content should be wrapped with Padding for consistent design.
-            Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 16.0),
-              child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.start, // Ensures left alignment
-                children: [
-                  // Product Name and Icons
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.name,
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.loyalty_outlined,
-                                color: colorScheme.primary),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.favorite_border,
-                                color: colorScheme.primary),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  // Product Category
-                  Text(
-                    product.category,
-                    style: TextStyle(color: colorScheme.primary),
-                  ),
-
-                  SizedBox(height: 10),
-
-                  // Star Rating and Review Count
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.star_border, color: colorScheme.primary),
-                          SizedBox(width: 4),
-                          Text(
-                            "${product.rating} stars",
-                            style: TextStyle(
-                                color: colorScheme.primary, fontSize: 12),
-                          )
-                        ],
-                      ),
-                      Text(
-                        "(${product.reviewCount} reviews)",
-                        style: TextStyle(fontSize: 12),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 15),
-
-                  // Product Description
-                  Text(
-                    product.description,
-                    style: TextStyle(
-                        color: colorScheme.onSurface.withOpacity(0.7)),
-                  ),
-                  SizedBox(height: 20),
-
-                  // Price
-                  Text(
-                    "Price",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  Divider(thickness: 1, color: Colors.black),
-
-                  // Product Prices
-                  Wrap(
-                    alignment: WrapAlignment.start,
-                    spacing: 10.0,
-                    children: product.prices.map((price) {
-                      return ConstrainedBox(
-                        constraints: BoxConstraints(
-                          maxWidth: 60.0,
+        appBar: AppBar(),
+        body: ListView(children: [
+          if (widget.product.image.isNotEmpty)
+            Container(
+              height: 150.0,
+              child: Image.asset(widget.product.image, fit: BoxFit.cover),
+            ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 4.0, 16.0, 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.product.name,
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.loyalty_outlined,
+                              color: colorScheme.primary),
+                          onPressed: () {},
                         ),
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 4.0),
-                          decoration: BoxDecoration(
-                            color: colorScheme.tertiary,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(price.size),
-                              Divider(thickness: 1, color: Colors.black),
-                              Text('\$${price.value.toStringAsFixed(2)}'),
-                            ],
-                          ),
+                        IconButton(
+                          icon: Icon(Icons.favorite_border,
+                              color: colorScheme.primary),
+                          onPressed: () {},
                         ),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  widget.product.category,
+                  style: TextStyle(color: colorScheme.primary),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.star_border, color: colorScheme.primary),
+                        SizedBox(width: 4),
+                        Text(
+                          "${widget.product.rating} stars",
+                          style: TextStyle(
+                              color: colorScheme.primary, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "(${widget.product.reviewCount} reviews)",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15),
+                Text(
+                  widget.product.description,
+                  style:
+                      TextStyle(color: colorScheme.onSurface.withOpacity(0.7)),
+                ),
+                SizedBox(height: 20),
+                // Price
+                Text(
+                  "Price",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Divider(thickness: 1, color: Colors.black),
+                Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: 10.0,
+                  children: widget.product.prices.map((price) {
+                    return ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: 60.0,
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 4.0),
+                        decoration: BoxDecoration(
+                          color: colorScheme.tertiary,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(price.size),
+                            Divider(thickness: 1, color: Colors.black),
+                            Text('\$${price.value.toStringAsFixed(2)}'),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Reviews",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Divider(thickness: 1, color: Colors.black),
+                BlocBuilder<ReviewBloc, ReviewState>(
+                  builder: (context, state) {
+                    if (state is ReviewLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (state is ReviewsLoaded) {
+                      return ListView.builder(
+                        itemCount: state.reviews.length,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) =>
+                            ProductReviewWidget(review: state.reviews[index]),
                       );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 20),
-
-                  // Reviews Title
-                  Text(
-                    "Reviews",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  ),
-                  Divider(thickness: 1, color: Colors.black),
-
-                  BlocBuilder<ReviewBloc, ReviewState>(
-                    builder: (context, state) {
-                      if (state is ReviewLoading) {
-                        return Center(child: CircularProgressIndicator());
-                      } else if (state is ReviewsLoaded) {
-                        return ListView.builder(
-                          itemCount: state.reviews.length,
-                          shrinkWrap:
-                              true, // Essential for inside another ListView
-                          physics:
-                              NeverScrollableScrollPhysics(), // To prevent nested scrolling
-                          itemBuilder: (context, index) =>
-                              ProductReviewWidget(review: state.reviews[index]),
-                        );
-                      } else {
-                        return Center(child: Text('Something went wrong!'));
-                      }
-                    },
-                  ),
-                ],
-              ),
-            )
-          ])),
+                    } else {
+                      return Center(child: Text('Something went wrong!'));
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        ]),
+        bottomNavigationBar: BottomNavBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
