67545d9b90ff76e7eefb9a2fd75e5fe5de58fdb7
Joshua-Frazier <joshua.frazier1519@gmail.com>
2023-08-18 11:31:46 -0600

    feat(develop): resolved merge conflicts

--- reefer_review_mobile/lib/presentation/post/general_post_widget.dart
+++ reefer_review_mobile/lib/presentation/post/general_post_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:expandable_text/expandable_text.dart';
 import '../../presentation/shared/rounded_container.dart';
@@ -5,7 +6,9 @@ import '../../data/post/general_post.dart';
 
 class GeneralPostWidget extends StatelessWidget {
   final GeneralPost generalPost;
+
   const GeneralPostWidget({super.key, required this.generalPost});
+
   @override
   Widget build(BuildContext context) {
     var colorScheme = Theme.of(context).colorScheme;
@@ -51,14 +54,20 @@ class GeneralPostWidget extends StatelessWidget {
               ? SizedBox(
                   height: 200,
                   width: double.infinity,
-                  child: Image.asset(
-                    generalPost.image,
-                    fit: BoxFit.cover,
-                  ),
+                  child: _displayImage(generalPost.image),
                 )
               : Container(),
         ],
       ),
     );
+  }
+
+  Widget _displayImage(String imagePath) {
+    // Check if the path is for bundled assets or file
+    if (imagePath.startsWith('assets/')) {
+      return Image.asset(imagePath, fit: BoxFit.cover);
+    } else {
+      return Image.file(File(imagePath), fit: BoxFit.cover);
+    }
   }
 }
