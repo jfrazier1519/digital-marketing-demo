25d8652967c0e5ba3fb705c185eb5552ca09baa7
Ryan Bitner <79929655+ryanbitner01@users.noreply.github.com>
2023-08-23 22:33:24 -0600

    fix(brands): Fixed cell

--- reefer_review_mobile/lib/presentation/brands_screen/brands_widget.dart
+++ reefer_review_mobile/lib/presentation/brands_screen/brands_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:reefer_review_mobile/presentation/brands_screen/brand_details_screen/brand_details_screen.dart';
 import '../../data/models/brand/brand.dart';
-import '../../data/models/route_arguments/brand_details_screen_arguments.dart';
 import '../shared/category_bubble.dart';
 
 class BrandWidget extends StatelessWidget {
@@ -13,109 +11,87 @@ class BrandWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var colorScheme = Theme.of(context).colorScheme;
 
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          BrandDetailsScreen.route,
-          arguments: BrandDetailsScreenArguments(brand: brand),
-        );
-      },
-      child: Container(
-        margin: const EdgeInsets.fromLTRB(15.0, 8.0, 0.0, 8.0),
-        decoration: BoxDecoration(
-          color: colorScheme.tertiary,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(8.0),
-            bottomLeft: Radius.circular(8.0),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: colorScheme.onBackground.withOpacity(0.1),
-              offset: const Offset(0, 2),
-              blurRadius: 4.0,
-            )
-          ],
+    return Container(
+      decoration: BoxDecoration(
+        color: colorScheme.secondaryContainer,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(4),
+          bottomLeft: Radius.circular(4),
         ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            brand.profileImage!.isNotEmpty
-                ? ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8.0),
-                      bottomLeft: Radius.circular(8.0),
-                    ),
-                    child: Image.asset(
-                      brand.profileImage!,
-                      width: 100,
-                      height: 125,
-                      fit: BoxFit.fill,
-                    ),
-                  )
-                : const SizedBox(width: 100, height: 100),
-            const SizedBox(width: 20),
-            Expanded(
+      ),
+      margin: const EdgeInsets.only(left: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(4),
+              bottomLeft: Radius.circular(4),
+            ),
+            child: Image.asset(
+              brand.profileImage!,
+              width: 125,
+              height: 150,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              height: 150,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  // Name
                   Text(
-                    brand.profileName!,
+                    brand.profileName ?? '',
                     style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
                   ),
-
-                  const SizedBox(height: 10),
-
+                  const SizedBox(
+                    height: 5,
+                  ),
                   Row(
+                    mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
                         children: [
-                          Icon(Icons.star_border,
-                              color: colorScheme.primary, size: 18),
-                          const SizedBox(
-                            width: 5,
-                          ),
+                          const Icon(Icons.star),
                           Text(
-                            brand.rating.toString(),
+                            '${brand.rating} stars',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                                color: colorScheme.primary),
+                              fontWeight: FontWeight.bold,
+                              color: colorScheme.primary,
+                            ),
                           ),
-                          Text(" Stars",
-                              style: TextStyle(
-                                  color: colorScheme.primary,
-                                  fontWeight: FontWeight.bold))
                         ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10.0),
-                        child: Text(
-                          '(${brand.reviewCount} reviews)',
-                        ),
-                      ),
+                      Text('(${brand.reviewCount}) Reviews')
                     ],
                   ),
-
-                  const SizedBox(height: 10),
+                  const SizedBox(
+                    height: 10,
+                  ),
                   Wrap(
                     spacing: 8.0,
                     runSpacing: 8.0,
-                    children: brand.categories
-                        .map((category) => CategoryBubble(
-                            text: category.toString().split('.').last,
-                            colorScheme: colorScheme))
-                        .toList(),
-                  )
+                    children: List.generate(
+                      brand.categories.length,
+                      (index) => CategoryBubble(
+                          text: brand.categories[index]
+                              .toString()
+                              .split('.')
+                              .last,
+                          colorScheme: colorScheme),
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
